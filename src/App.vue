91384e91a9bd62cf1e2022cf07725a5ddc88efcd
PAPATIRI <syariftaufik11@gmail.com>
2024-11-25 13:54:17 +0800

    setup vue js using shadcn-vue and tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/ui/button/Button.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <Button variant="default">Button Shadcn</Button>
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
