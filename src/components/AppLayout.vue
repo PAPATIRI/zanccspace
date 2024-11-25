a215a4226430a8d249f4200c9c84507276b1e3bd
PAPATIRI <syariftaufik11@gmail.com>
2024-11-25 21:18:13 +0800

    finished setup sidebar application

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import SidebarProvider from './ui/sidebar/SidebarProvider.vue'
+import AppSidebar from '@/components/AppSidebar.vue'
+import SidebarTrigger from './ui/sidebar/SidebarTrigger.vue'
+</script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <SidebarTrigger />
+      <slot />
+    </main>
+  </SidebarProvider>
+</template>
---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import Sidebar from './ui/sidebar/Sidebar.vue'
+import SidebarContent from './ui/sidebar/SidebarContent.vue'
+import SidebarGroup from './ui/sidebar/SidebarGroup.vue'
+import SidebarGroupContent from './ui/sidebar/SidebarGroupContent.vue'
+import SidebarGroupLabel from './ui/sidebar/SidebarGroupLabel.vue'
+import SidebarHeader from './ui/sidebar/SidebarHeader.vue'
+import SidebarMenu from './ui/sidebar/SidebarMenu.vue'
+import SidebarMenuItem from './ui/sidebar/SidebarMenuItem.vue'
+</script>
+
+<template>
+  <Sidebar variant="floating">
+    <SidebarContent>
+      <SidebarHeader>ZANCCSPACE</SidebarHeader>
+      <SidebarGroup>
+        <SidebarGroupLabel>Hobby</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <RouterLink to="/">Home</RouterLink>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <RouterLink to="/about">About</RouterLink>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SidebarContent>
+  </Sidebar>
+</template>
